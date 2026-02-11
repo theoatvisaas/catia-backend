87d8190ed8a35e90540f93417fcd2eeb19e54c97
Douglas Silva <douglascomercial03@gmail.com>
2026-02-11 17:37:18 -0300

    Implementando rota /me

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,10 +4,12 @@ import { loginController } from "../controllers/auth/loginController";
 import { requireAuth } from "../middlewares/requireAuth";
 import { changePasswordController } from "../controllers/auth/changePasswordController";
 import { refreshController } from "../controllers/auth/refreshController";
+import { meController } from "../controllers/auth/meController";
 
 export const authRoutes = Router();
 
 authRoutes.post("/signup", signupController);
 authRoutes.post("/login", loginController);
+authRoutes.get("/me", requireAuth, meController); 
 authRoutes.put("/change-password", requireAuth, changePasswordController);
 authRoutes.post("/refresh", refreshController);
